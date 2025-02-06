17090201ba8fbf20136ec960f9823a5a68cefb9b
Mdsakib999 <sakibabrar999@gmail.com>
2025-02-05 18:37:57 -0700

    Link change on allProducts

--- src/Pages/OnSale/OnSale.jsx
+++ src/Pages/OnSale/OnSale.jsx
@@ -8,7 +8,7 @@ const OnSale = () => {
       <h1 className="text-3xl font-bold mt-8 text-center">Product on Sale</h1>
       <div className="flex flex-col items-center justify-center flex-grow">
         <p className="text-lg text-gray-400 flex items-center gap-x-2 mb-20"><TbShoppingBagX size={34} /> Currently no product on sale.</p>
-        <Link to="/allProduct" className="px-6 py-3 uppercase mb-4 rounded-lg font-semibold text-white  transition-all duration-500 bg-gradient-to-r from-[#03b8e1] via-[#112949] to-[#00c4f5] bg-[length:200%_auto] shadow-lg hover:bg-right">Return to Shop</Link>
+        <Link to="/allProducts" className="px-6 py-3 uppercase mb-4 rounded-lg font-semibold text-white  transition-all duration-500 bg-gradient-to-r from-[#03b8e1] via-[#112949] to-[#00c4f5] bg-[length:200%_auto] shadow-lg hover:bg-right">Return to Shop</Link>
       </div>
     </div>
   );
